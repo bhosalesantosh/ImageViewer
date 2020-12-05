cee9d2bb657b9c7a14fd1d8c8fd05c910854d4cb
bhosalesantosh <bhosalesg@gmail.com>
2020-12-05 18:40:35 +0530

    Added changes in JSON files and added header and index files.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import AppRouter from "./common/router";
+import "./index.css";
+import { AppProvider } from "./common/app-context";
 
-ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
-    document.getElementById('root')
-);
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [searchKey, setSearchKey] = useState("");
+
+  return (
+    <AppProvider
+      value={{
+        searchKey,
+        setSearchKey,
+        isLoggedIn,
+        setIsLoggedIn
+      }}
+    >
+      <AppRouter />
+    </AppProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
